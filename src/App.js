126696c0cbbd48a98756d18b25fe6fb31b3c8a91
Adam Baumeister <abigserve@gmail.com>
2021-09-04 11:13:10 +1000

    Updated lots

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import {SBSLinter} from './lib/Linter';
+import './scss/custom.scss'
+import { SBSLinter } from './lib/Linter';
+
+function Title() {
+  return (
+    <div className="title row align-items-center justify-content-center">
+      <div className="row">
+        <div className="col">
+          <h1 className="display-1">Step-By-Step Markup Language</h1>
+          <h6>A simple markup language for describing technical or business processes.</h6>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-blockquote-left" viewBox="0 0 16 16">
+            <path d="M2.5 3a.5.5 0 0 0 0 1h11a.5.5 0 0 0 0-1h-11zm5 3a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1h-6zm0 3a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1h-6zm-5 3a.5.5 0 0 0 0 1h11a.5.5 0 0 0 0-1h-11zm.79-5.373c.112-.078.26-.17.444-.275L3.524 6c-.122.074-.272.17-.452.287-.18.117-.35.26-.51.428a2.425 2.425 0 0 0-.398.562c-.11.207-.164.438-.164.692 0 .36.072.65.217.873.144.219.385.328.72.328.215 0 .383-.07.504-.211a.697.697 0 0 0 .188-.463c0-.23-.07-.404-.211-.521-.137-.121-.326-.182-.568-.182h-.282c.024-.203.065-.37.123-.498a1.38 1.38 0 0 1 .252-.37 1.94 1.94 0 0 1 .346-.298zm2.167 0c.113-.078.262-.17.445-.275L5.692 6c-.122.074-.272.17-.452.287-.18.117-.35.26-.51.428a2.425 2.425 0 0 0-.398.562c-.11.207-.164.438-.164.692 0 .36.072.65.217.873.144.219.385.328.72.328.215 0 .383-.07.504-.211a.697.697 0 0 0 .188-.463c0-.23-.07-.404-.211-.521-.137-.121-.326-.182-.568-.182h-.282a1.75 1.75 0 0 1 .118-.492c.058-.13.144-.254.257-.375a1.94 1.94 0 0 1 .346-.3z" />
+          </svg>
+          <h2>Simple to Write!</h2>
+          <p>It only takes a minute to jot down a process, or multiple processes, in an SBSML compatible way.
+            <br></br>
+            Use it to keep your process definitions consistent, or to kickstart automation.
+          </p>
+        </div>
+        <div className="col">
+          <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-braces" viewBox="0 0 16 16">
+            <path d="M2.114 8.063V7.9c1.005-.102 1.497-.615 1.497-1.6V4.503c0-1.094.39-1.538 1.354-1.538h.273V2h-.376C3.25 2 2.49 2.759 2.49 4.352v1.524c0 1.094-.376 1.456-1.49 1.456v1.299c1.114 0 1.49.362 1.49 1.456v1.524c0 1.593.759 2.352 2.372 2.352h.376v-.964h-.273c-.964 0-1.354-.444-1.354-1.538V9.663c0-.984-.492-1.497-1.497-1.6zM13.886 7.9v.163c-1.005.103-1.497.616-1.497 1.6v1.798c0 1.094-.39 1.538-1.354 1.538h-.273v.964h.376c1.613 0 2.372-.759 2.372-2.352v-1.524c0-1.094.376-1.456 1.49-1.456V7.332c-1.114 0-1.49-.362-1.49-1.456V4.352C13.51 2.759 12.75 2 11.138 2h-.376v.964h.273c.964 0 1.354.444 1.354 1.538V6.3c0 .984.492 1.497 1.497 1.6z" />
+          </svg>
+          <h2>Parsable into code</h2>
+          <p>
+            Convert your processes and steps straight to code.
+            <br></br>
+            A standard format, and definitions for process inputs, outputs and flow logic makes it easy to use your processes in logic.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <SBSLinter/>
+    <div className="App container-fluid">
+      <Title />
+      <SBSLinter />
     </div>
   );
 }
--- src/lib/Linter.js
+++ src/lib/Linter.js
@@ -1,44 +1,126 @@
 import React from 'react';
 import {ProcessNode, SBSMLParser, StepNode} from 'sbsmljs/lib/parser';
 
+const EXAMPLE_SBS = `--- Bake a cake ---
+1. Butter > Melt some butter >> melted butter
+    1a. Put the cake in a small dish
+    1b. Melt in the microwave
+2. Mix the butter with the cake Mix
+This makes the icing mix good.
+3. Put the cake in the oven
+4. Wait 30 minutes
+5. Take the cake out of the oven
+then: Make the icing
+
+--- Make the icing ---
+1. Melt some more butter
+2. Mix the butter with the icing and milk
+3. Spread the icing on the cake!
+`
+
 export class SBSLinter extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {};
+      this.setParser = this.setParser.bind(this);
+      this.setParserError = this.setParserError.bind(this);
+
+      this.state = {
+          parser: null,
+          parserError: null
+      };
     }
   
+    setParser(newParser) {
+        this.setState({
+            parser: newParser
+        });
+    }
+
+    setParserError(err) {
+        console.log(err);
+        this.setState({
+            parserError: err
+        })
+    }
+
     render() {
         return (
-        <form>
-            <TextEditor/>
-        </form>
+        <div className="row main">
+            <div className="col p-0" style={{display: "contents"}}>
+                <form className="editorMainForm">
+                    <TextEditor updateParser={this.setParser} setParserError={this.setParserError}/>
+                </form>
+            </div>
+            <div className="col main-col border">
+                <ParsedDisplay parser={this.state.parser} parserError={this.state.parserError}/>
+            </div>
+        </div>
         );
     }
 }
 
-  class TextEditor extends React.Component {
+class TextEditor extends React.Component {
     constructor(props) {
       super(props);
       this.handleChange = this.handleChange.bind(this);
+      this.handleScroll = this.handleScroll.bind(this);
+
       this.state = {
-          value: ""
+          value: EXAMPLE_SBS,
+          scrollTopValue: "",
       };
+      this.myRef = React.createRef();
+    }
+
+    componentDidMount() {
+        var newParser = SBSMLParser.parse(this.state.value);
+        this.props.updateParser(newParser);
     }
     handleChange(event) {
-        //var parser = SBSMLParser.parse(event.target.value);
-        this.setState({value: event.target.value});
+        try {
+            var newParser = SBSMLParser.parse(event.target.value);
+        }
+        catch(err) {
+            this.props.setParserError(err)
+            return;
+        }
+        this.props.updateParser(newParser);
+        this.setState({
+            value: event.target.value,
+        });
+    }
+    handleScroll() {
+        this.setState({
+            scrollTopValue: this.myRef.current.scrollTop,
+            scrollLeftValue: this.myRef.current.scrollLeft,
+
+        })
     }
     render() {
         return (
         <div className="editorMain">
-            <textarea value={this.state.value} onChange={this.handleChange}/>
-            <TextDisplay text={this.state.value}/>
+            <TextDisplay text={this.state.value} scrollTop={this.state.scrollTopValue} scrollLeft={this.state.scrollLeftValue}/>
+            <textarea onScroll={this.handleScroll} ref={this.myRef} className="invis text-nowrap" value={this.state.value} onChange={this.handleChange}/>
         </div>
         );
     }
 }
 
 class TextDisplay extends React.Component {
+    constructor(props) {
+        super(props);
+        this.myRef = React.createRef();
+    }
+
+    componentDidUpdate() {
+        if (this.myRef.current) {
+            console.log("Setting values " + this.props.scrollTop + this.props.scrollLeft)
+
+            this.myRef.current.scrollTop = this.props.scrollTop;
+            this.myRef.current.scrollLeft = this.props.scrollLeft;
+        }
+    }
+
     parse(text) {
         var lines = text.split(/\r?\n/);
         var fmtLines = [];
@@ -49,7 +131,6 @@ class TextDisplay extends React.Component {
                     fmtLines.push(<ProcessLine text={line} key={index}/>)
                     break;
                 case StepNode:
-
                     fmtLines.push(<StepLine text={line} key={index}/>)
                     break;
                 default:
@@ -63,7 +144,7 @@ class TextDisplay extends React.Component {
     render() {
         var fmtText = this.parse(this.props.text);
         return (
-            <pre className="editorPre">{fmtText}</pre>
+            <pre ref={this.myRef} scrollTop={this.props.scrollTop} scrollLeft={this.props.scrollLeft} className="editorPre">{fmtText}</pre>
         );
     }
 }
@@ -82,4 +163,79 @@ class StepLine extends React.Component {
             <div className="yellowHighlight">{this.props.text}</div>
         )
     }
+}
+
+class ParsedDisplay extends React.Component {
+    render() {
+        if (this.props.parserError != null) {
+            return (
+                <div className="parsedDisplay pt-3 w-100">
+                    <ErrorDisplay parserError={this.props.parserError}/>
+                </div>
+            )            
+        }
+
+        var parser = this.props.parser; 
+        var processNodeDisplays = [];
+        if (parser !== null) {
+            parser.processNodes.forEach((pnode, i) => {
+                processNodeDisplays.push(<ProcessNodeDisplay key={i} node={pnode}/>)
+            })
+        }
+        return (
+            <div className="parsedDisplay pt-3 w-100">
+                {processNodeDisplays}
+            </div>
+        )
+    }
+}
+
+class ProcessNodeDisplay extends React.Component {
+    render() {
+        var stepNodes = [];
+        this.props.node.stepNodes.forEach((snode, i) => {
+            stepNodes.push(<StepNodeDisplay key={i} node={snode}/>)
+        })
+        return (
+            <div className="left-align">
+                <h2 className="rounded p-2 bg-light">{this.props.node.nodeName}</h2>
+                {stepNodes}
+            </div>
+        )
+    }   
+}
+
+class StepNodeDisplay extends React.Component {
+    render() {
+        return (
+            <div>
+                <h5 className="stepNode">{this.props.node.nodeName}
+                <br/>
+                </h5>
+                <DescriptionDisplay descriptionNode={this.props.node.descriptionNode}/>
+            </div>
+
+        )
+    }
+}
+
+class DescriptionDisplay extends React.Component {
+    render() {
+        if (this.props.descriptionNode !== undefined) {
+            return (
+                <small className="text-muted">{this.props.descriptionNode.text}</small>
+            )
+        }
+
+        return;
+    }
+}
+
+class ErrorDisplay extends React.Component {
+    render() {
+        return (
+            <h1 className="text-warning">Failed to parse {this.props.parserError.message}</h1>
+        )
+
+    }    
 }
